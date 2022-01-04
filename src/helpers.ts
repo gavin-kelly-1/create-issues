fedb0759ed6ed4cecc5e6c4f57d18996e8e8d439
Gavin Kelly <gavin.kelly@crick.ac.uk>
2022-01-04 10:43:08 +0000

    Install toposort dependency

--- src/helpers.ts
+++
@@ -1,9 +0,0 @@
-import { Toolkit } from 'actions-toolkit'
-import { IssuesCreateResponseData } from '@octokit/types'
-
-
-
-export function listToArray (list?: string[] | string) {
-  if (!list) return []
-  return Array.isArray(list) ? list : list.split(', ')
-}
